2fcdac60aa58551afb08f531e99b112ae570ee2a
darshan <dptarale@gmail.com>
2013-07-09 14:48:01 +0530

    added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 
 describe PagesController do
-
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-  end
-
+      it "should be successful" do
+        get 'home'
+        response.should be_success
+      end
   describe "GET 'contact'" do
+      it "should be successful" do
+        get 'contact'
+        response.should be_success
+      end
+  describe "GET 'about'" do
     it "should be successful" do
-      get 'contact'
+      get 'about'
       response.should be_success
     end
   end
